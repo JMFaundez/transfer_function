dc6a9b42c7bdba48d091e74efe6f2ef2a8fc4cf8
josework <josfa@mech.kth.se>
2023-11-06 14:24:31 +0100

    first commit

---
+++ gyz_1row.m
@@ -0,0 +1,57 @@
+function out = gyz_1row(ys,zs,times,cond)
+%% Parameters
+dtdns = 1e-5;
+z0 = linspace(-0.035,0.035,21);
+z0 = z0(1:end-1);
+NS = length(z0);
+
+
+w1 = 100;
+
+
+%% Compute Gyz
+
+res1 = ordinary_TF(ys,zs,z0,times,cond);
+
+%%
+
+Syy = res1.Syy;
+
+Syz = res1.Syz;
+
+% Gyz =  | Gyz_1   Gyz_2 |
+Sx = step_2(res1.ft*2*pi,w1,200,1e12,1e4);
+Sx = ifftshift(Sx);
+R2 = min(Sx);
+SxM = repmat(Sx',[1 20]);
+SxM = SxM/R2;
+SxM_inv = 1./SxM;
+
+
+
+%Gyz_1 = (Sy1z./Sy1y1 + Sy2z./Sy1y2).*SxM_inv;
+%Gyz_2 = (Sy1z./Sy2y1 + Sy2z./Sy2y2).*SxM_inv;
+
+Gyz = Syz./Syy.*SxM_inv;
+
+
+gyz = ifft(Gyz,[],1);
+gyz = ifft(gyz,[],2);
+
+
+
+
+z_s = conv_jose(ys,real(gyz),NS,length(times));
+
+
+
+
+error = mse(z_s-zs)/mse(zs);
+
+out.error = error;
+out.est = z_s;
+out.gyz = gyz;
+out.Gyz = Gyz;
+
+
+end
