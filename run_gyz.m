b6a7569c7d2fcc81785aea26def3d3029cd01b28
joselaptop <josfa@kth.se>
2023-11-06 15:20:20 +0100

    BOS

--- run_gyz.m
+++ run_gyz.m
@@ -7,10 +7,14 @@ cond.tap = 1; % Use hanning window or not
 
 
 
+%% Data Diego
+tic
+LD = load("../Linear_Diego/wallStresses.mat",'Q','t');
+toc
 gridDiego = load("../CF/Linear_Diego/grid");
 
 
 [Zd,Xd] = meshgrid(gridDiego.Z,gridDiego.X(:,1)); 
 [Ns,Nz] = size(Xd);
-
-
+time_diego = LD.t;
+[Np,Nt] = size(LD.Q);
