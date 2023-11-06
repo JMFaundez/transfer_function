fd8772d3b2939a080b43dd30b090dada58a56635
joselaptop <josfa@kth.se>
2023-11-06 15:11:50 +0100

    BOS

---
+++ run_gyz.m
@@ -0,0 +1,16 @@
+clear all
+
+% Welch parameters
+cond.q = 0.75; %overlap
+cond.nd = 10;  % Number of bins 
+cond.tap = 1; % Use hanning window or not
+
+
+
+gridDiego = load("../cf_beta/Linear_Diego/grid");
+
+
+[Zd,Xd] = meshgrid(gridDiego.Z,gridDiego.X(:,1)); 
+[Ns,Nz] = size(Xd);
+
+
