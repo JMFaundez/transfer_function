308a508c70cfdc6680988c9173fe4d47a0ea1fc1
joselaptop <josfa@kth.se>
2023-11-06 17:21:31 +0100

    BOS

--- run_gyz.m
+++ run_gyz.m
@@ -27,49 +27,53 @@ ixf = find(Xd(:,1)>=xf,1,"first");
 
 cf = (LD.Q(Np/3+1:2*Np/3,:));
 cf = reshape(cf,Ns,Nz,Nt);
-I = squeeze(cf(ix0,:,:))';
-O = squeeze(cf(ixf,:,:))';
+I = squeeze(cf(ix0,1:end-1,:))';
+O = squeeze(cf(ixf,1:end-1,:))';
 
-[Z,T] = meshgrid(gridDiego.Z,time_diego);
+[Z,T] = meshgrid(gridDiego.Z(1:end-1),time_diego);
 
-val=0.5e-3;
-cmap=parula;
-figure('Position',[500 500 1000 400])
-ax1 =subplot(211);
-pcolor(T,Z,I)
-shading interp
-clim([-val,val])
-colormap(ax1,cmap)
-colorbar()
+z = gridDiego.Z(1:end-1);
+out = gyz_1row(I,O,cond,time_diego,z);
+
+
+%val=0.5e-3;
+%cmap=parula;
+%figure('Position',[500 500 1000 400])
+%ax1 =subplot(211);
+%pcolor(T,Z,I)
+%shading interp
+%clim([-val,val])
+%colormap(ax1,cmap)
+%colorbar()
+%%axis equal
+%ylim([min(Z(:)),max(Z(:))])
+%%xlim([0.02,0.5])
+%title('Input Linear')
+%ylabel('$z$','FontSize',18,'Interpreter','Latex')
+%xlabel('time','FontSize',18,'Interpreter','Latex')
+%
+%ax2 =subplot(212);
+%pcolor(T,Z,O)
+%shading interp
+%clim([-val,val])
+%colormap(ax1,cmap)
+%colorbar()
+%%axis equal
+%ylim([min(Z(:)),max(Z(:))])
+%%xlim([0.02,0.5])
+%title('Output Linear')
+%ylabel('$z$','FontSize',18,'Interpreter','Latex')
+%xlabel('time','FontSize',18,'Interpreter','Latex')
+%
+%cfLD = squeeze(cf(:,:,600));
+%figure('Position',[500 500 1000 400])
+%ax1 =subplot(211);
+%pcolor(Xd,Zd,cfLD)
+%shading interp
+%clim([-val,val])
+%colormap(ax1,cmap)
+%colorbar()
 %axis equal
-ylim([min(Z(:)),max(Z(:))])
+%ylim([min(Z(:)),max(Z(:))])
 %xlim([0.02,0.5])
-title('Input Linear')
-ylabel('$z$','FontSize',18,'Interpreter','Latex')
-xlabel('time','FontSize',18,'Interpreter','Latex')
-
-ax2 =subplot(212);
-pcolor(T,Z,O)
-shading interp
-clim([-val,val])
-colormap(ax1,cmap)
-colorbar()
-%axis equal
-ylim([min(Z(:)),max(Z(:))])
-%xlim([0.02,0.5])
-title('Output Linear')
-ylabel('$z$','FontSize',18,'Interpreter','Latex')
-xlabel('time','FontSize',18,'Interpreter','Latex')
-
-cfLD = squeeze(cf(:,:,600));
-figure('Position',[500 500 1000 400])
-ax1 =subplot(211);
-pcolor(Xd,Zd,cfLD)
-shading interp
-clim([-val,val])
-colormap(ax1,cmap)
-colorbar()
-axis equal
-ylim([min(Z(:)),max(Z(:))])
-xlim([0.02,0.5])
-ylabel('$z$','FontSize',18,'Interpreter','Latex')
+%ylabel('$z$','FontSize',18,'Interpreter','Latex')
