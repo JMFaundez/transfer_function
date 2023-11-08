13390c2fac91f46db7a63e749987d6aa5f55f8f2
joselaptop <josfa@kth.se>
2023-11-08 14:18:59 +0100

    BOS

--- data_vel/read_hpts.m
+++ data_vel/read_hpts.m
@@ -1,21 +1,17 @@
-function read_hpts(casei)
-%clear all
+clear all
 
+casei=1;
 switch casei
 case 1
-	fnameopen = "FST_BF.his";
-	filename = "mesh1_BF/FST25_BF_";
-	time_steps = 1;
+	fnameopen = "FST.his";
+	filename = "Linear_vel";
+	time_steps = 480+480+480+480+480;
 	iinit=0;
-	nx=58;
-	ny=50;
-case 2
-	fnameopen = "FST_unc.his";
-	filename = "mesh1_unc/FST25_unc_";
-	time_steps = 161+160;
-	iinit=0;
-	nx=58;
-	ny=50;
+	nxLE=3;
+	nyLE=4;
+	nxBL=4;
+	nyBL=3;
+	nz = 150;
 end
 
 
@@ -33,10 +29,12 @@ xyz(:,1) = coord(1:3:end);
 xyz(:,2) = coord(2:3:end);
 xyz(:,3) = coord(3:3:end);
 
-
-nprobes = nx*ny
-nz = npoints/nprobes
-nplane = nz*ny
+NLE = nxLE*nyLE;
+NBL = nxBL*nyBL;
+nprobes=NLE+NBL;
+%nprobes = nx*ny
+%nz = npoints/nprobes
+%nplane = nz*ny
 
 
 %time_steps = 161;33; (length(A)-3*npoints-1)/(npoints*(n_fields+1));
@@ -48,42 +46,50 @@ vx = fields(2:(n_fields+1):end);
 vy = fields(3:(n_fields+1):end);
 vz = fields(4:(n_fields+1):end);
 pr = fields(5:(n_fields+1):end);
-nt = time_steps
+nt = time_steps;
 
+u1 = zeros(nt,nprobes,nz); u2 = zeros(nt,nprobes,nz); u3 = zeros(nt,nprobes,nz);
+xx = zeros(nprobes,nz); yy = zeros(nprobes,nz); zz = zeros(nprobes,nz);
+p = zeros(nt,nprobes,nz);
 
 %u1 = zeros(nt,nx,ny,nz); u2 = zeros(nt,nx,ny,nz); u3 = zeros(nt,nx,ny,nz);
 %xx = zeros(nx,ny,nz); yy = zeros(nx,ny,nz); zz = zeros(nx,ny,nz);
 %p = zeros(nt,nx,ny,nz);
 for t=1:nt
 	t
-for i=1:nx
-	index = (t-1)*nplane*nx+nplane*(i-1)+1:(t-1)*nplane*nx+nplane*i;
+for i=1:nprobes
+	%index = (t-1)*nplane*nx+nplane*(i-1)+1:(t-1)*nplane*nx+nplane*i;
+	index = (t-1)*nz*nprobes+nprobes*(i-1)+1:(t-1)*nz*nprobes+nprobes*i;
 	if t==1
 		inn=[index(1),index(end),index(end)-index(1)];
 	end
-%	u1(t,i,:,:) = reshape(vx(index),nz,ny)';	
-%	u2(t,i,:,:) = reshape(vy(index),nz,ny)';	
-%	u3(t,i,:,:) = reshape(vz(index),nz,ny)';
-%  p(t,i,:,:) = reshape(pr(index),nz,ny)';
+	u1(t,i,:) = reshape(vx(index),nz,ny)';	
+	u2(t,i,:) = reshape(vy(index),nz,ny)';	
+	u3(t,i,:) = reshape(vz(index),nz,ny)';
+  	p(t,i,:) = reshape(pr(index),nz,ny)';
 
-	v1 = reshape(vx(index),nz,ny)';	
-	v2 = reshape(vy(index),nz,ny)';	
-	v3 = reshape(vz(index),nz,ny)';
-  p = reshape(pr(index),nz,ny)';
-	fnamei = filename+num2str(i)+"_it_"+num2str(t+iinit)
-	save(fnamei,'v1','v2','v3','p','-v7.3')
-	disp("saving file "+fnamei)
+%	v1 = reshape(vx(index),nz,ny)';	
+%	v2 = reshape(vy(index),nz,ny)';	
+%	v3 = reshape(vz(index),nz,ny)';
+%  	p = reshape(pr(index),nz,ny)';
+	%fnamei = filename+num2str(i)+"_it_"+num2str(t+iinit)
+	%save(fnamei,'v1','v2','v3','p','-v7.3')
+	%disp("saving file "+fnamei)
 end
 end
 
 %disp('Saving files...')
 
-for i=1:nx
-	index = nplane*(i-1)+1:nplane*i;
-	xx = reshape(xyz(index,1),nz,ny)';	
-	yy = reshape(xyz(index,2),nz,ny)';	
-	zz = reshape(xyz(index,3),nz,ny)';	
-	save(filename+num2str(i)+"_mesh",'xx','yy','zz')
+%xx = zeros(nprobes,nz);
+%yy = zeros(nprobes,nz);
+%zz = zeros(nprobes,nz);
+
+for i=1:nprobes
+	index = nz*(i-1)+1:nz*i;
+	xx(i,:) = reshape(xyz(index,1),nz,ny)';	
+	yy(i,:) = reshape(xyz(index,2),nz,ny)';	
+	zz(i,:) = reshape(xyz(index,3),nz,ny)';	
+%	save(filename+num2str(i)+"_mesh",'xx','yy','zz')
 %	for it=1:nt
 %	v1 = squeeze(u1(it,i,:,:));
 %	v2 = squeeze(u2(it,i,:,:));
@@ -97,6 +103,7 @@ end
 
 %%
 
+save(filename,'u1','v2','u3','p','xx','yy','zz','-v7.3')
 disp('files SHOULD be saved...')
 disp('================== END PROGRAM ======================')
 
@@ -127,4 +134,3 @@ disp('================== END PROGRAM ======================')
 %    save(filename,'XYZ')
 %end
 %toc
-end
