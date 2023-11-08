72b884c32ca762d5b28ea25394d005d10e8905e0
joselaptop <josfa@kth.se>
2023-11-08 14:31:55 +0100

    BOS

--- data_vel/plot_vel.m
+++ data_vel/plot_vel.m
@@ -1,3 +1,20 @@
 clear all
 
 L = load('Linear_vel');
+iin = 1;
+iout = 13;
+
+uin = squeeze(L.u1(:,iin,:));
+zin = squeeze(L.z(iin,:));
+t = L.time;
+
+
+[T,Z] = meshgrid(t,zin);
+
+figure()
+surf(T,Z,uin')
+shading interp
+view(2)
+colorbar()
+
+
