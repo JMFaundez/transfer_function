dc6f69ec33a7ae63b4a86141f1b37f10e4275ee3
joselaptop <josfa@kth.se>
2023-11-08 14:34:36 +0100

    BOS

--- data_vel/plot_vel.m
+++ data_vel/plot_vel.m
@@ -6,15 +6,25 @@ iout = 13;
 
 uin = squeeze(L.u1(:,iin,:));
 zin = squeeze(L.zz(iin,:));
+uout = squeeze(L.u1(:,iout,:));
 t = L.time;
 
 
 [T,Z] = meshgrid(t,zin);
 
-figure()
+figure('Position',[300 300 1000 300])
 surf(T,Z,uin')
 shading interp
+xlim([min(t),max(t)])
+ylim([min(zin),max(zin)])
 view(2)
 colorbar()
 
 
+figure('Position',[300 300 1000 300])
+surf(T,Z,uout')
+shading interp
+xlim([min(t),max(t)])
+ylim([min(zin),max(zin)])
+view(2)
+colorbar()
